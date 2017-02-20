6f01f59f6dce81de52e9994e6a5234a2a2c2d605
James Talarico <james.v.talarico@gmail.com>
2017-02-20 15:54:16 -0500

    EP-88 #comment Finished preliminary add money. Values are still hard-coded.

--- app/Http/Controllers/Payments/BankTransferController.php
+++ app/Http/Controllers/Payments/BankTransferController.php
@@ -14,6 +14,8 @@ use PayPal\Api\Payment;
 use PayPal\Api\RedirectUrls;
 use PayPal\Api\Transaction;
 
+use PayPal\Api\PaymentExecution;
+
 class BankTransferController extends Controller
 {
     /**
@@ -95,15 +97,15 @@ class BankTransferController extends Controller
 
 
 // For Sample Purposes Only.
-        $request = clone $payment;
+        //$request = clone $payment;
 
-// ### Create Payment
-// Create a payment by calling the 'create' method
-// passing it a valid apiContext.
-// (See bootstrap.php for more on `ApiContext`)
-// The return object contains the state and the
-// url to which the buyer must be redirected to
-// for payment approval
+        // ### Create Payment
+        // Create a payment by calling the 'create' method
+        // passing it a valid apiContext.
+        // (See bootstrap.php for more on `ApiContext`)
+        // The return object contains the state and the
+        // url to which the buyer must be redirected to
+        // for payment approval
         try {
             $payment->create($paypal->getContext());
         } catch (Exception $ex) {
@@ -111,17 +113,82 @@ class BankTransferController extends Controller
             exit(1);
         }
 
-// ### Get redirect url
-// The API response provides the url that you must redirect
-// the buyer to. Retrieve the url from the $payment->getApprovalLink()
-// method
-        $approvalUrl = $payment->getApprovalLink();
+        // ### Get redirect url
+        // The API response provides the url that you must redirect
+        // the buyer to. Retrieve the url from the $payment->getApprovalLink()
+        // method.
 
         return $payment;
     }
 
-    public function handleCallback(Request $request)
+    public function handleCallback(PayPalServiceProvider $paypal, Request $request)
     {
+        //dd($request->all());
+
+        if ($request->success == 'true') {
+
+            // Get the payment Object by passing paymentId
+            // payment id was previously stored in session in
+            // CreatePaymentUsingPayPal.php
+            $paymentId = $request->paymentId;
+            $payment = Payment::get($paymentId, $paypal->getContext());
+
+            // ### Payment Execute
+            // PaymentExecution object includes information necessary
+            // to execute a PayPal account payment.
+            // The payer_id is added to the request query parameters
+            // when the user is redirected from paypal back to your site
+            $execution = new PaymentExecution();
+            $execution->setPayerId($request->PayerID);
+
+            // ### Optional Changes to Amount
+            // If you wish to update the amount that you wish to charge the customer,
+            // based on the shipping address or any other reason, you could
+            // do that by passing the transaction object with just `amount` field in it.
+            // Here is the example on how we changed the shipping to $1 more than before.
+            $transaction = new Transaction();
+            $amount = new Amount();
+            $details = new Details();
+
+            $details->setShipping(2.2)
+                ->setTax(1.3)
+                ->setSubtotal(17.50);
+
+            $amount->setCurrency('USD');
+            $amount->setTotal(21);
+            $amount->setDetails($details);
+            $transaction->setAmount($amount);
+
+            // Add the above transaction object inside our Execution object.
+            $execution->addTransaction($transaction);
+
+            try {
+                // Execute the payment
+                // (See bootstrap.php for more on `ApiContext`)
+                $result = $payment->execute($execution, $paypal->getContext());
+
+                try {
+                    $payment = Payment::get($paymentId, $paypal->getContext());
+                } catch (Exception $ex) {
+                    // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+                    //ResultPrinter::printError("Get Payment", "Payment", null, null, $ex);
+                    exit(1);
+                }
+            } catch (Exception $ex) {
+                // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+                //ResultPrinter::printError("Executed Payment", "Payment", null, null, $ex);
+                exit(1);
+            }
+
+            // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+            //ResultPrinter::printResult("Get Payment", "Payment", $payment->getId(), null, $payment);
+
+            return $payment;
+        } else {
+            // NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+            //ResultPrinter::printResult("User Cancelled the Approval", null);
+            exit;
+        }
 
     }
 
