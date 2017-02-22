d96735ba9ae28eef4f828157796bd238d0d58b8e
Alessandro Power <alessandrojpower@gmail.com>
2017-02-22 15:58:13 -0500

    Added BankTransfer class.

---
+++ database/migrations/2017_02_22_202955_create_bank_transfers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBankTransfersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_transfers', function (Blueprint $table) {
+            $table->increments('id');
+	    $table->string('method');
+	    $table->string('invoice_id');
+	    $table->string('amount');
+            $table->string('status');
+	    $table->integer('wallet_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_transfers');
+    }
+}
