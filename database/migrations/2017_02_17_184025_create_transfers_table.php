cf2ef75b7d69e8d4428373a1cdba1fb84ee705fd
Alessandro Power <alessandrojpower@gmail.com>
2017-02-22 14:03:52 -0500

    Completed migration table for Transfer.

---
+++ database/migrations/2017_02_17_184025_create_transfers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransfersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->increments('id');
+	    $table->integer('sender_wallet_id');
+	    $table->integer('receiver_wallet_id');
+	    $table->string('amount');
+	    $table->string('status');
+	    $table->string('token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transfers');
+    }
+}
