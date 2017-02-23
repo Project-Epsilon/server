c9e0439856e90242a9dc1a31c0e35e43a068ccf9
James Talarico <james.v.talarico@gmail.com>
2017-02-23 13:08:12 -0500

    Deleted unnessisary file

--- app/Http/Controllers/Transfer/Bank/DepositController.php
+++ app/Http/Controllers/Transfer/Bank/DepositController.php
@@ -9,7 +9,6 @@ use App\Providers\PayPalServiceProvider;
 
 use PayPal\Exception\PayPalConnectionException;
 use PayPal\Api\Amount;
-use PayPal\Api\Details;
 use PayPal\Api\Item;
 use PayPal\Api\ItemList;
 use PayPal\Api\Payer;
