b5fe20494b0395ce579b496f85afac033b61a737
Laurendy Lam <lamlaurendy@gmail.com>
2017-03-04 14:35:54 -0500

    EP-37 EP-42 Delete contact and test.

--- tests/feature/ContactTest.php
+++ tests/feature/ContactTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Contact;
 use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -13,13 +14,13 @@ class ContactTest extends TestCase
     use DatabaseMigrations, WithoutMiddleware;
 
     /**
-     * A basic test example.
+     * Storing contact test.
      *
      * @return void
      */
     public function testStore()
     {
-        $this->seed();
+        $this->seed(\UsersTableSeeder::class);
         $this->be(User::find(1));
 
         $this->post('api/user/contact', [
@@ -29,5 +30,23 @@ class ContactTest extends TestCase
             'name' => 'John Smith',
             'phone_number' => 5145554444
         ]);
+    }
+
+    /**
+     * Destroy contact test.
+     */
+    public function testDestroy()
+    {
+        $this->seed(\UsersTableSeeder::class);
+
+        $user = User::find(1);
+        $this->be($user);
+
+        $user->contacts()->save(new Contact([
+            'name' => 'Timothy Doe',
+            'phone_number' => 5145554444
+        ]));
+
+        $this->delete('api/user/contact/1')->assertSee('ok');
     }
 }
