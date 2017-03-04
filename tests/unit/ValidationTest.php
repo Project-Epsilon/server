617f5d5878a8c42409490e94b99caa86ca8352b0
Laurendy Lam <lamlaurendy@gmail.com>
2017-03-03 20:43:09 -0500

    integrate greater than validation rule

---
+++ tests/unit/ValidationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Validator;
+
+class ValidationTest extends TestCase
+{
+    /**
+     * A greater than validation rule test.
+     *
+     * @return void
+     */
+    public function testGreaterThanRule()
+    {
+        $validator = Validator::make([
+            'value' => 0
+        ], [
+            'value' => 'required|greater_than:0'
+        ]);
+
+        $this->assertTrue($validator->fails());
+
+        $validator = Validator::make([
+            'value' => 1
+        ], [
+            'value' => 'required|greater_than:0'
+        ]);
+
+        $this->assertFalse($validator->fails());
+    }
+}
