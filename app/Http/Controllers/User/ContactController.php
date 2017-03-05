1f348ea94e6415dcbe23fa6c50d3e88389e683e5
Laurendy Lam <lamlaurendy@gmail.com>
2017-03-05 18:32:09 -0500

    Delete contact (#32)
    * upgrate laravel to 5.3 / update testing
    
    * remove auth0 authentication method and use laravel socialite
    
    * re-integrate jwt library
    
    * refactor tests - integrate json error response handling
    
    * integrate greater than validation rule
    
    * EP-41 Add contacts and test
    
    * EP-37 EP-42 Delete contact and test.
    
    * correcting contact id
    
    * EP-37 EP-42 Delete contact and test.
    
    * correcting contact id

--- app/Http/Controllers/User/ContactController.php
+++ app/Http/Controllers/User/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Contact;
 use App\Transformers\ContactTransformer;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -58,17 +59,26 @@ class ContactController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Contact $contact
+     * @param Request $request
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\JsonResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function destroy($id)
+    public function destroy($id, Request $request)
     {
-        //
+        $contact = Contact::find($id);
+
+        if(! $contact || ! $this->canEditContact($contact, $request->user())){
+            return $this->sendErrorResponse();
+        }
+
+        $contact->delete();
+
+        return response('ok');
     }
 
 
     /**
-     * Validates the inputs of the requests
+     * Validates the inputs of the requests.
      *
      * @param Request $request
      */
@@ -78,6 +88,32 @@ class ContactController extends Controller
             'name' => 'required',
             'phone_number' => 'sometimes',
             'email' => 'sometimes|required|email'
+        ]);
+    }
+
+    /**
+     * Determines if a user can modify this contact.
+     *
+     * @param Contact $contact
+     * @param User $user
+     * @return bool
+     */
+    protected function canEditContact(Contact $contact, User $user)
+    {
+        return $contact->user_id == $user->id;
+    }
+
+    /**
+     * Sends the not found contact error response.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function sendErrorResponse()
+    {
+        return response()->json([
+            'errors' => [
+                'message' => 'Contact not found.'
+            ]
         ]);
     }
 
