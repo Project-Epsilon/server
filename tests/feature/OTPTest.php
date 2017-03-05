dbe1e092a95153ef3e2efde28ff156a69dfcdc7e
Laurendy Lam <lamlaurendy@gmail.com>
2017-03-05 11:38:40 -0500

    EP-110 test otp validation

--- tests/feature/OTPTest.php
+++ tests/feature/OTPTest.php
@@ -35,4 +35,35 @@ class OTPTest extends TestCase
             'phone_number' => ''
         ])->assertSee('errors');
     }
+
+    /**
+     * Validates token.
+     *
+     * @return void
+     */
+    public function testValidation()
+    {
+        $this->seed();
+
+        $user = User::find(1);
+        $this->be($user);
+
+        $this->post('api/auth/otp', [
+            'phone_number' => 15149275582
+        ])->assertSee('ok');
+
+        $user->fresh();
+
+        $this->post('api/auth/otp/unlock', [
+            'token' => $user->otp - 1
+        ])->assertSee('errors');
+
+        $this->post('api/auth/otp/unlock', [
+            'token' => $user->otp
+        ])->assertSee('ok');
+
+        $user = User::find(1);
+
+        $this->assertNull($user->otp);
+    }
 }
