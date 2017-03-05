0864479801003b8d20707a5703a551acf84e2cec
Laurendy Lam <lamlaurendy@gmail.com>
2017-03-05 10:45:01 -0500

    EP-109 add OTPController.php with request method

---
+++ app/Http/Controllers/Auth/OTPController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Providers\NexmoServiceProvider;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class OTPController extends Controller
+{
+    /**
+     * Sends an otp token to the requested phone number.
+     *
+     * @param Request $request
+     * @param NexmoServiceProvider $nexmo
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\JsonResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function request(Request $request, NexmoServiceProvider $nexmo)
+    {
+        $this->validate($request, [
+            'phone_number' => 'required'
+        ]);
+
+        $user = $request->user();
+        $user->update([
+            'phone_number' => $request->phone_number,
+            'otp' => rand(100000, 999999)
+        ]);
+
+        try {
+            $nexmo->send('Your verification number is ' . $user->otp, $request->phone_number);
+        } catch (\Exception $e){
+            return response()->json([
+                'errors' => [
+                    'message' => 'There was an error with the phone number.'
+                ]
+            ]);
+        }
+
+        return response('ok');
+    }
+}
