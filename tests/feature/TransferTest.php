42679872a2c596c7ac5929af38b788e6575439bb
Laurendy Lam <lamlaurendy@gmail.com>
2017-03-08 22:53:36 -0500

    send transfer w/ test

--- tests/feature/TransferTest.php
+++ tests/feature/TransferTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Transfer;
 use App\User;
+use App\Wallet;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -27,13 +28,51 @@ class TransferTest extends TestCase
 
         $this->post('api/transfer/user/send', [
             'receiver'  => [
-                'phone_number' => '15143338888',
             ],
             'amount' => 1.21,
             'wallet_id' => 1,
             'message' => 'Hello you are a friendly person'
         ])->assertSee('errors');
 
-        $this->assertNotNull(Transfer::find(1));
+        $this->post('api/transfer/user/send', [
+            'receiver'  => [
+                'phone_number' => '15143338888',
+            ],
+            'amount' => 100.01,
+            'wallet_id' => 1,
+            'message' => 'Hello you are a friendly person'
+        ])->assertSee('errors');
+
+        $this->post('api/transfer/user/send', [
+            'receiver'  => [
+                'phone_number' => '15143338888',
+            ],
+            'amount' => 100,
+            'wallet_id' => 2,
+            'message' => 'Hello you are a friendly person'
+        ])->assertSee('errors');
+
+        $this->post('api/transfer/user/send', [
+            'receiver'  => [
+                'phone_number' => '15143338888',
+            ],
+            'amount' => 100,
+            'wallet_id' => 2,
+            'message' => 'Hello you are a friendly person'
+        ])->assertSee('errors');
+
+
+        $this->post('api/transfer/user/send', [
+            'receiver'  => [
+                'phone_number' => '15143338888',
+            ],
+            'amount' => 100,
+            'wallet_id' => 1,
+            'message' => 'Hello you are a friendly person'
+        ])->assertJsonStructure([
+            'data' => ['id', 'sender', 'message']
+        ]);
+
+        $this->assertEquals(Wallet::find(1)->balance, '0');
     }
 }
