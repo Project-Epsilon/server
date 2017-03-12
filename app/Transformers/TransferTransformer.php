bd1323c767df3d1e56029bd417a5c8814b6a4906
Laurendy Lam <lamlaurendy@gmail.com>
2017-03-12 18:22:39 -0400

    add amount to transfer

--- app/Transformers/TransferTransformer.php
+++ app/Transformers/TransferTransformer.php
@@ -19,6 +19,7 @@ class TransferTransformer extends TransformerAbstract
             'id' => $transfer->id,
             'sender' => $transfer->senderWallet->user()->select('name')->first()->name, //TODO: Performance twice
             'message' => $transfer->message,
+            'amount' => $transfer->amount,
             'receiver' => [
                 'email' => $transfer->receiver_email,
                 'phone_number' => $transfer->receiver_phone_number
