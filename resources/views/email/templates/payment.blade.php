cc5bb0a6b099769deace6cca497cf157b4ee46f7
Laurendy Lam <lamlaurendy@gmail.com>
2017-03-23 17:07:45 -0400

    mail template

---
+++ resources/views/email/templates/payment.blade.php
@@ -0,0 +1,17 @@
+@extends('email.layout')
+
+@section('title')
+    You have received a new payment.
+@endsection
+
+@section('message')
+    <h4>Hi {{ $receiver }},</h4>
+    <p>Hi Bob, you've received a amount of {{ $amount }} from {{ $sender }}. Token#: {{ $token }} </p>
+    @if(strlen($text) > 0)
+        <h4>Message: </h4>
+        <p>{{ $text }}</p>
+    @endif
+    <p class="text-center">
+        <a class="btn" href="">Click here to claim.</a>
+    </p>
+@endsection
