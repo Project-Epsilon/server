fdc3b1af7452b29f67e0bbef851897b0fe94f4ec
Laurendy Lam <lamlaurendy@gmail.com>
2017-04-01 15:20:00 -0400

    Transfers api (#67)
    * added relations to model
    
    * update transfer database
    
    * implement transfers controller
    
    * add route
    
    * transfers test

---
+++ tests/feature/UserTransferTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Transfer;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserTransferTest extends TestCase
+{
+    use DatabaseMigrations, WithoutMiddleware;
+
+    /**
+     * A get all transfers tests.
+     *
+     * @return void
+     */
+    public function testGetAllTransfer()
+    {
+        $this->seed();
+
+        $user = User::find(1);
+        $this->be($user);
+
+        $wallet = $user->wallets()->first();
+
+        $wallet->transfersOut()
+            ->save(factory(Transfer::class)->make());
+        $wallet->transfersIn()
+            ->save(factory(Transfer::class)->make([
+                'receiver' => 'Bob'
+            ]));
+
+        $this->get('api/transfer/user/transfer')
+            ->assertSee('data');
+    }
+
+    /**
+     * Get a transfer by token
+     *
+     * @return void
+     */
+    public function testGetTransferByToken()
+    {
+        $this->seed();
+
+        $token = factory(Transfer::class)->create()->token;
+
+        $this->post('api/transfer/user', [
+            'token' => $token
+        ])->assertSee('data');
+    }
+
+    /**
+     * Gets a transfer by id.
+     *
+     * @return void
+     */
+    public function testGetATransferTest()
+    {
+        $this->seed();
+        $this->be(User::find(1));
+
+        $transfer = factory(Transfer::class)->create([
+            'sender_wallet_id' => 1
+        ]);
+
+        $this->get('api/transfer/user/transfer/' . $transfer->id)
+            ->assertSee('data');
+    }
+
+}
