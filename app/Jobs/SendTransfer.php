5046ad5f71af3b327017ae6ae28d9b278f3527c4
Laurendy Lam <lamlaurendy@gmail.com>
2017-04-07 12:52:30 -0400

    add deeplink

--- app/Jobs/SendTransfer.php
+++ app/Jobs/SendTransfer.php
@@ -50,9 +50,11 @@ class SendTransfer implements ShouldQueue
             Mail::to($email)->send(new NewPayment($this->transfer));
         }
 
+        $link = config('app.transfer_link') . $this->transfer->token;
+
         if ($phone_number) {
             $message = (new NexmoMessage())
-                ->content('You\'ve got a new payment of ' . $amount . ' ' . $code. '. ' . $this->transfer->token . ' ')
+                ->content('You\'ve got a new payment of ' . $amount . ' ' . $code. '. ' . $link . ' ')
                 ->to($phone_number);
 
             try {
