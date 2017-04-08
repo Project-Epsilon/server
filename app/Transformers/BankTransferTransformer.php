f9db8f2818eff6c63c25bffc64d3c4fe70ee6551
Laurendy Lam <lamlaurendy@gmail.com>
2017-04-08 18:31:59 -0400

    bank transfer transformer

---
+++ app/Transformers/BankTransferTransformer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Transformers;
+
+use App\BankTransfer;
+use League\Fractal\TransformerAbstract;
+
+class BankTransferTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(BankTransfer $transfer)
+    {
+        return [
+            'method' => $transfer->method,
+            'invoice_id' => $transfer->invoice_id,
+            'amount' => $transfer->amount_display,
+            'status' => $transfer->complete,
+            'incoming' => $transfer->incoming
+        ];
+    }
+}
